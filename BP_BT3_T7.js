0075e65a5713fae000dffa79fbdd2fee15123205
caongan <caothuongngan@gmail.com>
2021-04-07 16:53:47 +0700

    baitap3

---
+++ BP_BT3_T7.js
@@ -0,0 +1,8 @@
+let R
+let PI=3.14;
+R = +prompt("nhập vào bán kính(m):");
+S=R*R*PI;
+P=R*2*PI;
+document.write("-Diện tích: " + S+"m2");
+document.write("<br\>");
+document.write("-Chu vi: " + P+"m");
