20716ea7c7ebc2f76d25ee20d44f53ed87c641fd
caongan <caothuongngan@gmail.com>
2021-04-07 15:33:45 +0700

    baitap

---
+++ BT_T7.js
@@ -0,0 +1,11 @@
+let a
+let b
+let c
+a = +prompt("điểm vật lí");
+b = +prompt("điểm hoá học");
+c = +prompt("điểm sinh học");
+DTB=(a+b+c)/3;
+DT=(a+b+c);
+document.write("DTB: " +DTB);
+document.write("<br\>");
+document.write("DT: " +DT);
